f4bde4865a5de49abf85ae1a6fecdf478b9baeb1
JangGwon <tlsakch510@naver.com>
2022-11-27 01:14:13 +0900

    하단바 추가

--- app/src/main/java/com/example/mylittleassist/MainActivity.java
+++ app/src/main/java/com/example/mylittleassist/MainActivity.java
@@ -1,14 +1,51 @@
 package com.example.mylittleassist;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    HomeFragment homeFragment;
+    ListFragment listFragment;
+    CalanderFragment calanderFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        homeFragment = new HomeFragment();
+        listFragment = new ListFragment();
+        calanderFragment = new CalanderFragment();
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,homeFragment).commit();
+
+        NavigationBarView navigationBarView = findViewById(R.id.bottom_navigationview);
+        navigationBarView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId())
+                {
+                    case R.id.home:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,homeFragment).commit();
+                        return true;
+                    case R.id.list:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,listFragment).commit();
+                        return true;
+                    case R.id.calendar:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,calanderFragment).commit();
+                        return true;
+                }
+
+                return false;
+            }
+        });
     }
 }
