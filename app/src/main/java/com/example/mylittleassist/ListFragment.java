b63d33d6c9070decb78703e07cf8a0d4b16635f7
JangGwon <tlsakch510@naver.com>
2022-11-27 17:05:39 +0900

    리스트 뷰 구현 중

--- app/src/main/java/com/example/mylittleassist/ListFragment.java
+++ app/src/main/java/com/example/mylittleassist/ListFragment.java
@@ -1,12 +1,16 @@
 package com.example.mylittleassist;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.fragment.app.Fragment;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +18,8 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class ListFragment extends Fragment {
+
+    ArrayList<SampleData> movieDataList;
 
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
@@ -34,13 +40,36 @@ public class ListFragment extends Fragment {
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        onCreateView(R.layout.fragment_list);
+
+        this.InitializeMovieData();
+
+        ListView listView = (ListView)findViewById(R.id.listView);
+        final MyAdapter myAdapter = new MyAdapter(this,movieDataList);
+
+        listView.setAdapter(myAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView parent, View v, int position, long id){
+                Toast.makeText(getActivity().getApplicationContext(),
+                        myAdapter.getItem(position).getMovieName(),
+                        Toast.LENGTH_LONG).show();
+            }
+        });
     }
+
+    public void InitializeMovieData()
+    {
+        movieDataList = new ArrayList<SampleData>();
+
+        movieDataList.add(new SampleData(R.drawable.calendar, "미션임파서블","15세 이상관람가"));
+        movieDataList.add(new SampleData(R.drawable.clipboard, "아저씨","19세 이상관람가"));
+        movieDataList.add(new SampleData(R.drawable.home, "어벤져스","12세 이상관람가"));
+    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
